d430c5dc58df52160d238ca8e77bc3f65156cd17
loganhoadley <logan.hoadley@gmail.com>
2022-01-20 12:15:15 -0600

    Testing Seaborn integration and output functionality.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+#testing Seaborn library and its interaction in pycharm
+#show() needs to be called in order to explicitly draw the generated plot in the IDE
+#matplotlib.pyplot.ion() enables interactive mode, which should serve the final product
+
+import seaborn as sns
+import matplotlib as plt
+
+df= sns.load_dataset('penguins')
+sns.pairplot(df, hue="species")
+plt.pyplot.show()
