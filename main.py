33c63eaa88bcc865355ecc4059f86a2b5dd39a24
loganhoadley <logan.hoadley@gmail.com>
2022-02-08 00:14:32 -0600

    Program currently does not function as intended yet -- but a foundation exists. Currently, the list in the base menu does not update as I want it to. I may need to change the flow of how I expect the app to function, but the core functionality should work with the implementation I have in mind.

--- main.py
+++ main.py
@@ -1,34 +1,52 @@
 # matplotlib.pyplot.ion() enables interactive mode, which should serve the final product
+from tkinter import filedialog
+import tkinter as tk
+import csv
 # import seaborn as sns
 # import matplotlib as plt
 
-import tkinter as tk
-import csv
-from tkinter import filedialog
+from tkinter import *
+
+headerlist = ["sample"]
+data = []
 
 
 def openfile():
     filepath = filedialog.askopenfilename(initialdir="%USERPROFILE%/Downloads",
                                           filetypes=(("Comma Separated Value Lists", "*.csv"), ("All Files", "*.*")))
-
     with open(filepath, newline='') as csvfile:
+        data = []
         header = csvfile.readline()
         headerlist = header.split(',')
         reader = csv.reader(csvfile)
         for row in reader:
             data.append(row)
-
-    print(headerlist)
-    print(headerlist[4])
-    print(data[1][0])
+    print(headerlist)  # prints headerlist as it exists when created from .csv
+    print(data[1][0])  # 2nd column timestamp for debug purposes
 
 
-data = []
+# maybe just build a window in this open function that contains the graph,
+# having the options for selection show up in the new window
+# have the main window maybe contain some documentation and guiding the user? I'm not sure exactly how to get tkinter
+# to behave in the way I want, I'll play around with the order
+
 
 root = tk.Tk()
 root.title("PyGuilin Dev Build")
 
+
+def show():
+    label.config(text=clicked.get())
+
+
+clicked = StringVar()
+clicked.set("Parameter 1")
+
+drop = OptionMenu(root, clicked, headerlist)
+drop.pack()
+
 main_menu = tk.Menu(root)
+
 root.config(menu=main_menu)
 file_menu = tk.Menu(main_menu, tearoff=0)
 main_menu.add_cascade(label='File', menu=file_menu)
@@ -37,18 +55,7 @@ file_menu.add_command(label="Export as...")
 file_menu.add_separator()
 file_menu.add_command(label="Exit", command=root.quit)
 
+label = Label(root, text=" ")
+label.pack()
 
 root.mainloop()
-
-
-# root = tk.Tk()
-# root.withdraw()
-
-# data[] should contain a 2D array, which contains elements as lists with data associations from the .CSV
-#
-
-# data is now loaded
-
-
-# access in 2d array, 1st element is the row of the CSV, 2nd element is
-# the index of the precise entry, or use one element for the whole row
