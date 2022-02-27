81b52ef30d6decf10d36e78c737af383c91f34de
Logan Hoadley <logan.hoadley@gmail.com>
2022-02-27 00:57:01 -0600

    GUI works and limits are available with a graph that appears on the top layer when a graph is generated. Graphs can be opened and kept open or closed as need be, different files can be opened and graphs will remain open.
    TODO:
    --Fix data reading, .CSVs don't update at regular intervals but ~~every 15 seconds should be okay I think
    --intercepts with regression model returning as values?

--- main.py
+++ main.py
@@ -3,16 +3,28 @@ from tkinter import filedialog
 import tkinter as tk
 import seaborn as sns
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from tkinter import *
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg)
 sns.set_theme(color_codes=True)
 
-headerlist = ["sample"]
-data = []
-plt.pyplot.ion()
-global csv
-firstRun = True
 
+
+def openfile():
+    global filepath, headers, firstRun, csv
+    filepath = filedialog.askopenfilename(initialdir="%USERPROFILE%/Downloads",
+                                          filetypes=(("Comma Separated Value Lists", "*.csv"), ("All Files", "*.*")))
+    csv = pd.read_csv(filepath)
+    headers = list(csv.columns)
+    if firstRun:
+        startLabel.pack_forget()
+        firstRun=False
+    else:
+        undraw()
+    draw()
+
+#creates the buttons and visual elements necessary for interaction.
 def draw():
     global windowframe, buttonframe
     root.title(filepath)
@@ -30,44 +42,46 @@ def draw():
     drop2.pack(side=TOP)
     sliderlabel = Label(buttonframe, text="Width deviation limit in\n hundredths of millimeters: ")
     sliderlabel.pack(side=TOP)
-    slider = Scale(buttonframe, from_=0, to=500, orient=HORIZONTAL)
+    limit=DoubleVar()
+
+    slider = Scale(buttonframe, from_=0, to=75, orient=HORIZONTAL, variable=limit)
     slider.pack(side=TOP)
-    graphbutton = Button(buttonframe, text="Generate", command=lambda: graph(csv, clicked1.get(), clicked2.get()))
+    graphbutton = Button(buttonframe, text="Generate", command=lambda: graph(csv, clicked1.get(), clicked2.get(), limit.get()))
     graphbutton.pack(side=BOTTOM)
 
-
+#clears the frame when a new file is opened. called only after the first file is opened.
 def undraw():
     windowframe.pack_forget()
     buttonframe.pack_forget()
 
+#graphs
+def graph(file, arg1, arg2,arg3):
+    global canvas
+    graphwindow=Toplevel(height=900, width=1000)
+    fig = create_figure(file, arg1, arg2)
+    if arg3 != 0:
+        plt.axhline(y=(arg3/1000),color='r',linestyle='-')
+    canvas = FigureCanvasTkAgg(fig, master=graphwindow)
+    canvas.draw()
+    canvas.get_tk_widget().pack()
+    button = tk.Button(graphwindow, text="Quit", command=graphwindow.destroy)
+    button.pack()
 
-
-def graph(file, arg1, arg2):
+    # creates a figure containing the graph, type figure
+def create_figure(file, arg1, arg2):
+    f, dummy = plt.subplots(figsize=(6,6))
     sns.regplot(x=arg1, y=arg2, data=file)
-    plt.pyplot.show()
-    #  need this method to be superseded
-    #  graph =
-    #  graphic = FigureCanvasTkAgg(graph, graphwindow)
-    #  graphic.get_tk_widget().pack(side=LEFT, fill=BOTH)
+    return f
 
-
-def openfile():
-    global filepath, headers, firstRun, csv
-    filepath = filedialog.askopenfilename(initialdir="%USERPROFILE%/Downloads",
-                                          filetypes=(("Comma Separated Value Lists", "*.csv"), ("All Files", "*.*")))
-    csv = pd.read_csv(filepath)
-    headers = list(csv.columns)
-    if firstRun:
-        startLabel.pack_forget()
-        firstRun=False
-    else:
-        undraw()
-    draw()
+#Main begins here
+#plt.pyplot.ion()
+global csv
+firstRun = True
 
 root = tk.Tk()
 root.title("PyGuilin Dev Build")
 main_menu = tk.Menu(root)
-root.geometry("500x800")
+root.geometry("800x600")
 root.config(menu=main_menu)
 file_menu = tk.Menu(main_menu, tearoff=0)
 main_menu.add_cascade(label='File', menu=file_menu)
